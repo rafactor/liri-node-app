13fcac50432c420a811385fc7e89d9c01f875da2
rafactor <avila.rafael@gmail.com>
2018-12-06 21:44:30 -0500

    song search completed

--- songs.js
+++ songs.js
@@ -1,8 +1,6 @@
 var func = {
     search(query) {
 
-        console.log('song logic')
-
         // call spotify api
         require("dotenv").config();
         var keys = require("./keys");
@@ -16,59 +14,67 @@ var func = {
         //API call
         spotify.search({
           type: 'track',
-          query: 'let it be'
+          query: query
         }, function (err, data) {
           if (err) {
             return console.log('Error occurred: ' + err);
           }
       
-          console.log(data)
-          // var url = data.tracks.href;
-          // var limit = data.tracks.limit;
-          // var next = data.tracks.next;
-          // var previous = data.tracks.previous;
-          // var total = data.tracks.total;
+        //   console.log(data)
+          var url = data.tracks.href;
+          var limit = data.tracks.limit;
+          var next = data.tracks.next;
+          var previous = (data.tracks.previous === null)? 0:data.tracks.previous ;
+          var total = data.tracks.total;
       
-          // var items = data.tracks.items
+          var item = data.tracks.items
       
-          // console.log(`Found ${total} tracks matching '${name}':`);
-          // console.log(`Results from ${previous} to ${limit}':`);
-          // console.log(item[0])
+          console.log(`Found ${total} tracks matching '${query}':`);
+          console.log(`Results from ${previous} to ${limit}':`);
+  
+    
       
-          // console.log(items[0]);
+            // limit = 1
+          for (let i = 0; i < limit; i++) {
+           
+           
+            //track info
+            let artist = item[i].artists[0].name;
+            // let artists = item.album.artists; //array
+            let album = item[i].album.name;
+            let released = item[i].album.release_date;
+            let spotifyLink = item[i].album.external_urls.spotify;
+            let trackName = item[i].name
+
       
-          // items.forEach((index, item) => {
-          //   console.log(item)
-          //   //track info
-          //   let artistName = item.artists.name;
-          //   let artists = item.album.artists; //array
-          //   let name = item.album.name;
-          //   let href = item.album.href;
+            /* not in use
+            let albumType = item.album.albumType;
+            let artistType = item.artists.type;
+            let id = item.album.id;
+            let images = item.album.images;
+            let href = item.album.href;
+
+            let totalTracks = item.album.totalTracks;
+            
+            */
       
-          //   /* not in use
-          //   let albumType = item.album.albumType;
-          //   let artistType = item.artists.type;
-          //   let spotifyLink = item.album.external_urls.spotify;
-          //   let id = item.album.id;
-          //   let images = item.album.images;
-          //   let released = item.album.release_date;
-          //   let totalTracks = item.album.totalTracks;
-          //   let type = item.album.type;
-          //   let uri = item.album.uri;
-          //   */
+            let record = {
+             
+                'resultNo': (i + 1 < 10)? '0'+ (i + 1):(i + 1),
+                'artist': artist,
+              'album': album,
+              'released': released,
+            //   'list': artists,
+              'link':spotifyLink,
+              'track':trackName
+            }
       
-          //   let record = [{
-          //     'artist': artistName,
-          //     'track': name,
-          //     'released': released,
-          //     'list': artists,
-          //     'link':href,
-          //   }]
+            tracks.push(record)
+
+            console.log(' - ' + tracks[i].resultNo + ' | song: ' + tracks[i].track + ' | artist: ' + tracks[i].artist  + ' | album: ' + tracks[i].album + ' | released on ' + tracks[i].released + ' | link: ' + tracks[i].link)
+          };
       
-          //   tracks.push(record)
-          // });
-      
-          // console.log(tracks)
+          
       
         });
     }
